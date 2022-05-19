34ed6d164d40bb6c42167dc9826a6936349d6445
DESKTOP-D5NHP53\Maaz Javaid <mmaazj323@gmail.com>
2022-05-19 09:42:25 +0500

    setting up button

--- app/src/main/java/com/example/intentandactivities/MainActivity.java
+++ app/src/main/java/com/example/intentandactivities/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.intentandactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+//    public void callME(View view){
+//        Uri uri = Uri.parse("tel:+923001234567");
+//        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+//        startActivity(intent);
+//    }
+
+public void onClickButton(View v) {
+    if(v.getId() == R.id.yourbuttonid) {
+        Button btn=findViewById(R.id.yourbuttonid);
+        btn.setText("X");
+    }
+}
 }
